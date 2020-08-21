489b8ffb2301a7330678a1706c31b608784a0521
manish kushwaha <tzmanish97@gmail.com>
2020-08-21 12:52:44 +0530

    Admin-API - Migrations

---
+++ Middleware/StockMarket/AdminAPI/Migrations/20200821072142_Created_Tables.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdminAPI.Migrations
+{
+    public partial class Created_Tables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    CompanyCode = table.Column<string>(maxLength: 15, nullable: false),
+                    CompanyName = table.Column<string>(maxLength: 30, nullable: false),
+                    Turnover = table.Column<double>(nullable: false),
+                    CEO = table.Column<string>(maxLength: 30, nullable: true),
+                    Brief = table.Column<string>(nullable: true),
+                    Sector = table.Column<string>(maxLength: 30, nullable: true),
+                    BoardOfDirectors = table.Column<string>(nullable: true),
+                    StockExchanges = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.CompanyCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(maxLength: 30, nullable: false),
+                    Password = table.Column<string>(maxLength: 30, nullable: false),
+                    IsAdmin = table.Column<bool>(nullable: false),
+                    Email = table.Column<string>(maxLength: 30, nullable: true),
+                    Mobile = table.Column<string>(maxLength: 15, nullable: true),
+                    Confirmed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IPODetails",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyCode = table.Column<string>(maxLength: 15, nullable: false),
+                    StockExchange = table.Column<string>(maxLength: 30, nullable: false),
+                    PricePerShare = table.Column<double>(nullable: false),
+                    TotalShares = table.Column<long>(nullable: false),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    Remrks = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IPODetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IPODetails_Company_CompanyCode",
+                        column: x => x.CompanyCode,
+                        principalTable: "Company",
+                        principalColumn: "CompanyCode",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StockPrice",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyCode = table.Column<string>(maxLength: 15, nullable: false),
+                    StockExchange = table.Column<string>(maxLength: 30, nullable: false),
+                    CurrentPrice = table.Column<double>(nullable: false),
+                    Date = table.Column<DateTime>(type: "Date", nullable: false),
+                    Time = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockPrice", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StockPrice_Company_CompanyCode",
+                        column: x => x.CompanyCode,
+                        principalTable: "Company",
+                        principalColumn: "CompanyCode",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IPODetails_CompanyCode",
+                table: "IPODetails",
+                column: "CompanyCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockPrice_CompanyCode",
+                table: "StockPrice",
+                column: "CompanyCode");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IPODetails");
+
+            migrationBuilder.DropTable(
+                name: "StockPrice");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Company");
+        }
+    }
+}
